c7a67ca9b077f4bfa8502c77c054e232a7816954
Michael Kennedy <merpso@gmail.com>
2020-06-02 18:40:40 +1000

    updates thing

--- modules/functions/jumphost/vars.tf
+++ modules/functions/jumphost/vars.tf
@@ -1,7 +1,3 @@
-variable "cidr" {
-
-}
-
 variable "prefix" {
 
 }
@@ -26,31 +22,10 @@ variable "keyname" {
 
 }
 
-variable "keyfile" {
-
-}
 variable "vpcid" {
 
 }
 
-variable "region" {}
-
 variable "allowed_mgmt_cidr" {
   default = "0.0.0.0/0"
-}
-
-variable "allowed_app_cidr" {
-  default = "0.0.0.0/0"
-}
-
-variable "public_nic_ids" {}
-
-variable "docker_private_ip" {}
-
-variable "bigip_mgmt_addr" {}
-variable "bigip_mgmt_dns" {}
-variable "bigip_private_add" {}
-variable "bigip_password" {}
-variable "internal_subnet_offset" {
-  default = 20
 }
