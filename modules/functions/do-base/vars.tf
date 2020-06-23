2ed0e9c66b673ab027f226e1cd702707208168d3
Michael Kennedy <merpso@gmail.com>
2020-06-24 03:21:08 +1000

    DO Resources (maybe)

--- modules/functions/do-base/vars.tf
+++ modules/functions/do-base/vars.tf
@@ -1,14 +1,11 @@
 variable "bigip_mgmt_public_ip" {
-  type = string
   description = "public ip's"
 }
 
 variable "bigip_mgmt_admin" {
-  type = string
   description = "admin"
 }
 
 variable "bigip_mgmt_passwd" {
-  type = string
   description = "admin"
 }
